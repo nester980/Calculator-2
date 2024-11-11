027427a43d5c628db02f9bd52a7e954f88b5095f
nester980 <nester980@gmail.com>
2024-11-11 12:49:19 +0300

    Calculator final 2

--- js/script.js
+++ js/script.js
@@ -1,33 +1,39 @@
 let display = document.querySelector('.display');
-
 let buttons = Array.from(document.querySelectorAll(".button"));
 
-buttons.map((button) => {
-  button.addEventListener(click, (e) => {
+buttons.forEach(button => {
+  button.addEventListener("click", (e) => {
     switch (e.target.innerText) {
       case "AC":
         display.innerText = 0;
         break;
       case "=":
         try {
-          display.innerText = eval(display.innerText);
+          const result = calculateResult(display.innerText);
+          display.innerText = result.toString();
         } catch (e) {
           display.innerText = "Error";
         }
         break;
       case "+/-":
-        display.innerText = "-";
+        const currentValue = parseFloat(display.innerText);
+        display.innerText = (currentValue * -1).toString();
         break;
       case "%":
-        let passedText = display.innerText + "/100";
-        display.innerText = eval(passedText);
+        let value = parseInt(display.innerText) / 100;
+        display.innerText = value.toString();
         break;
       default:
         if (display.innerText === "0" && e.target.innerText !== ".") {
           display.innerText = e.target.innerText;
         } else {
           display.innerText += e.target.innerText;
-        }  
+        }
     }
-  })
+  });
 });
+
+// Функция для безопасного вычисления выражения
+function calculateResult(expression) {
+  return Function('return ' + expression)();
+}
